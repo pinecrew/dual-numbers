699746f732744f7a3471c6e413cd543eb904983d
Vova Abdrakhmanov <369565@gmail.com>
2017-07-11 16:13:22 +0300

    [add] more functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::ops::{AddAssign, SubAssign, MulAssign, DivAssign};
 use std::ops::{Add, Sub, Mul, Div, Neg};
-use std::f64;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Dual<T> {
@@ -9,8 +8,11 @@ pub struct Dual<T> {
 }
 
 impl<T> Dual<T> {
-    pub fn new(real: f64, dual: T) -> Dual<T> {
-        Dual { real, dual }
+    pub fn new<U: Into<f64>>(real: U, dual: T) -> Dual<T> {
+        Dual {
+            real: real.into(),
+            dual: dual,
+        }
     }
 
     pub fn sin(self) -> Dual<T>
@@ -22,9 +24,91 @@ impl<T> Dual<T> {
 
     pub fn cos(self) -> Dual<T>
     where
-        T: Mul<f64, Output = T> + Neg<Output = T>,
+        T: Mul<f64, Output = T>,
     {
-        Dual::new(self.real.cos(), -self.dual * self.real.sin())
+        Dual::new(self.real.cos(), self.dual * (-self.real.sin()))
+    }
+
+    pub fn tan(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.tan(), self.dual / self.real.cos().powi(2))
+    }
+
+    pub fn asin(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(
+            self.real.asin(),
+            self.dual / (1f64 - self.real.powi(2)).sqrt(),
+        )
+    }
+
+    pub fn acos(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(
+            self.real.acos(),
+            self.dual / (-(1f64 - self.real.powi(2)).sqrt()),
+        )
+    }
+
+    pub fn atan(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.atan(), self.dual / (1f64 + self.real.powi(2)))
+    }
+
+    pub fn sinh(self) -> Dual<T>
+    where
+        T: Mul<f64, Output = T>,
+    {
+        Dual::new(self.real.sinh(), self.dual * self.real.cosh())
+    }
+
+    pub fn cosh(self) -> Dual<T>
+    where
+        T: Mul<f64, Output = T>,
+    {
+        Dual::new(self.real.cosh(), self.dual * self.real.sinh())
+    }
+
+    pub fn tanh(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.tanh(), self.dual / self.real.cosh().powi(2))
+    }
+
+    pub fn asinh(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(
+            self.real.asinh(),
+            self.dual / (self.real.powi(2) + 1f64).sqrt(),
+        )
+    }
+
+    pub fn acosh(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(
+            self.real.acosh(),
+            self.dual / (self.real.powi(2) - 1f64).sqrt(),
+        )
+    }
+
+    pub fn atanh(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.atanh(), self.dual / (1f64 - self.real.powi(2)))
     }
 
     pub fn exp(self) -> Dual<T>
@@ -33,6 +117,73 @@ impl<T> Dual<T> {
     {
         let real_exp = self.real.exp();
         Dual::new(real_exp, self.dual * real_exp)
+    }
+
+    pub fn exp2(self) -> Dual<T>
+    where
+        T: Mul<f64, Output = T>,
+    {
+        let real_exp2 = self.real.exp2();
+        Dual::new(real_exp2, self.dual * real_exp2 * 2f64.ln())
+    }
+
+    pub fn ln(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.ln(), self.dual / self.real)
+    }
+
+    pub fn log(self, base: f64) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.log(base), self.dual / self.real / base.ln())
+    }
+
+    pub fn log2(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.log2(), self.dual / self.real / 2f64.ln())
+    }
+
+    pub fn log10(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.log10(), self.dual / self.real / 10f64.ln())
+    }
+
+    pub fn abs(self) -> f64 {
+        self.real
+    }
+
+    pub fn powi(self, n: i32) -> Dual<T>
+    where
+        T: Mul<f64, Output = T>,
+    {
+        Dual::new(
+            self.real.powi(n),
+            self.dual * (self.real.powi(n - 1) * n as f64),
+        )
+    }
+
+    pub fn powf(self, n: f64) -> Dual<T>
+    where
+        T: Mul<f64, Output = T>,
+    {
+        Dual::new(
+            self.real.powf(n),
+            self.dual * (self.real.powf(n - 1f64) * n),
+        )
+    }
+
+    pub fn sqrt(self) -> Dual<T>
+    where
+        T: Div<f64, Output = T>,
+    {
+        Dual::new(self.real.sqrt(), self.dual / self.real.sqrt() / 2f64)
     }
 }
 
